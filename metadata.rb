80f53ab696d9fbb9d91084f97409bc6f84b1d506
Eduard Giménez <eduard@emtrics.com>
2012-09-13 19:33:22 +0200

    Librarian needs a metadata file

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+maintainer       "Eduard Giménez"
+maintainer_email "eduard@emtrics.com"
+license          "Apache 2.0"
+description      "Recipes to install qrencode on Debian based distros."
+long_description "N/A"
+version          "0.1"
+
+
+supports "ubuntu"
+supports "debian"
